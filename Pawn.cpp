69a775f62f4db133f9b51ee50ff96c97ba865239
spiroskou <skoulouris77@gmail.com>
2024-05-02 16:07:06 +0300

    some indetaiton fixes

--- Pawn.cpp
+++ Pawn.cpp
@@ -11,19 +11,18 @@ bool Pawn::isValidMove(int org_row, int org_col, int trg_row, int trg_col) const
     std::shared_ptr<Piece> trg_piece = trg_block.getPiece();
 
 	if (trg_piece->getType() == PieceType::Empty) {
+
         // Pawn can move forward one square
         if (trg_col == org_col && trg_row == org_row + direction) {
             return true;
-        }
-        // Pawn can move forward two squares from its starting position
-        else if (trg_col == org_col && trg_row == org_row + 2 * direction && getFirstMove()) {
+        } else if (trg_col == org_col && trg_row == org_row + 2 * direction && getFirstMove()) { // Pawn can move forward two squares from its starting position
+
             // Check if the path is clear (no pieces in the way)
             if (board->getBlock(org_row + direction, org_col).isEmpty()) {
                 return true;
             }
         }
-    }
-    else {
+    } else {
         // Check if the destination block contains an opponent's piece
         if (board->getBlock(trg_row, trg_col).getPiece()->getColor() != this->getColor()) {
             // Pawn can capture diagonally
